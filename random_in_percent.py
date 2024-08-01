12f148c1212a4d6da55a07b3bbfa542328af3d51
KonstantinVaganov <105306858+KonstantinVaganov@users.noreply.github.com>
2024-08-01 20:50:10 +0700

    Add files via upload
    add best func

--- random_in_percent.py
+++ random_in_percent.py
@@ -1,4 +1,5 @@
 import random
+import timeit
 
 
 def choice_in_percent(percent, arg1, arg2):
@@ -16,4 +17,17 @@ def choice_in_percent(percent, arg1, arg2):
     return random.choice(result)
 
 
+def choice_in_percent2(percent, arg1, arg2):
+    ''' Для работы нужно импортировать модуль random.
+        percent = указать проценты
+        arg1 = с шансом (percent) выпадет arg1
+        arg2 = с шансом (100 - percent) выпадет arg2 '''
+    result = random.choice(range(1, 101))
+    if 1 <= result <= percent:
+        return arg1
+    else:
+        return arg2
+
+
 print(choice_in_percent(10, 'Шанс выпода 10%', 'Шанс выпода 90%'))
+print(choice_in_percent2(10, 'Шанс выпода 10%', 'Шанс выпода 90%'))
